c843743110a1405075ad33a5db5342e161df11f5
Pum <28806828+PumPum7@users.noreply.github.com>
2025-03-09 22:52:58 +0100

    Initial Commit

---
+++ Pomate/PomateApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomateApp.swift
+//  Pomate
+//
+//  Created by Michael D. on 09.03.25.
+//
+
+import SwiftUI
+
+@main
+struct PomateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PomateTests/PomateTests.swift
@@ -0,0 +1,17 @@
+//
+//  PomateTests.swift
+//  PomateTests
+//
+//  Created by Michael D. on 09.03.25.
+//
+
+import Testing
+@testable import Pomate
+
+struct PomateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
